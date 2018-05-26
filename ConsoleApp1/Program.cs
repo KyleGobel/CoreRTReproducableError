c85708a9917092d533c60113f521b6dd6f638631
Kyle <kgobel@gmail.com>
2018-05-26 17:58:29 -0400

    example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using YamlDotNet.Serialization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting process");
+            var yamlText = System.IO.File.ReadAllText("TestFile.yaml");
+            Console.WriteLine($"Read text:  {yamlText}");
+            var result = DeserializeModel(yamlText);
+            Console.WriteLine("Got the result");
+        }
+
+        static SimpleModel DeserializeModel(string yaml)
+        {
+            var deserializer = new DeserializerBuilder()
+                .IgnoreUnmatchedProperties()
+                .Build();
+
+            return deserializer.Deserialize<SimpleModel>(yaml);
+        }
+    }
+
+    public class SimpleModel
+    {
+        public SimpleModel()
+        {
+        }
+
+        [YamlMember(Alias = "kind", ApplyNamingConventions = false)]
+        public string Kind { get; set; }
+
+        [YamlMember(Alias = "apiVersion", ApplyNamingConventions = false)]
+        public string ApiVersion { get; set; }
+
+        [YamlMember(Alias = "currentContext", ApplyNamingConventions = false)]
+        public string CurrentContext { get; set; }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Reproduce
+
+```powershell
+git clone <this-repo> 
+cd <repo-directory>
+dotnet publish ConsoleApp1 -r win-x64 -c Debug -o ../Output
+cd Output
+./ConsoleApp1.exe
+```
+
